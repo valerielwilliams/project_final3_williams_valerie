8f8d99c65a581c6d8ff8cac45494eb76fa85c953
Valerie Williams <valerielwilliams@ufl.edu>
2017-04-16 12:52:11 -0400

    add smooth scroll jQuery plugin
    I added a smooth scroll jQuery plugin so that users are smoothly
    taken to the corresponding section upon clicking the link.

--- js/main.js
+++ js/main.js
@@ -4,3 +4,18 @@ $(function () {
         closeOnClick: true
     });
 });
+
+//smooth scrolling
+$(document).ready(function () {
+    $('a').on('click', function (event) {
+        if (this.hash !== '') {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
