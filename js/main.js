c8a146aa81c975c670e9aff4bdcb72a13a36bb84
Valerie Williams <valerielwilliams@ufl.edu>
2017-04-17 10:53:12 -0400

    change duration of logo animation

--- js/main.js
+++ js/main.js
@@ -24,6 +24,6 @@ $(document).ready(function () {
 //custom jQuery code - shrinks logo on hover
 
 $('#bobslogo').hover(function () {
-    $(this).animate({width: 175}, 1200);
-    $(this).animate({width: 200}, 1200);
+    $(this).animate({width: 175}, 1500);
+    $(this).animate({width: 200}, 1500);
 });
