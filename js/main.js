0e9b77d92a2793e890e2410c629d0e8be2a86ef4
Valerie Williams <valerielwilliams@ufl.edu>
2017-04-18 15:15:21 -0400

    add JavaScript code that reveals secret menu

--- js/main.js
+++ js/main.js
@@ -27,3 +27,13 @@ $('#bobslogo').hover(function () {
     $(this).animate({width: 175}, 1500);
     $(this).animate({width: 200}, 1500);
 });
+
+
+//custom JavaScript code - reveals the 'secret menu'
+
+var secretBurger = document.getElementById('bobslogo');
+secretBurger.addEventListener('dblclick', function () {   //This listens for a double-click on the Bob's Burgers logo.
+    var newParagraph = document.createElement('section'); //Upon hearing the double-click, a new section appears that reveals a secret menu item. Ooooh!
+    newParagraph.innerHTML = "<h2>Secret Menu</h2><h3 class='item'>Bob's Secret Burger<span class='price'>$6</span></h3>'<p class='description'>All-beef patty topped with Bob's Secret Sauce</p>"; //completes the sentence with 'for a good day'.
+    document.getElementsByTagName('section')[5].appendChild(newParagraph);
+});
